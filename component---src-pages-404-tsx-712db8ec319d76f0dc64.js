73da4a5e14b861da18d9dd382d61ca38fd7e81fd
alegemaate <alegemaate@gmail.com>
2021-02-24 02:19:46 +0000

    Deploying to gh-pages from  @ 53fc8276c34d97a896578df914874e8a0d97328d 🚀

---
+++ component---src-pages-404-tsx-712db8ec319d76f0dc64.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),o=n("VXBa"),l=n("pOKU");t.default=function(){return u.a.createElement(o.a,null,u.a.createElement(l.a,{title:"Page Not Found"}),u.a.createElement("h1",null,"Page not found!"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-712db8ec319d76f0dc64.js.map
